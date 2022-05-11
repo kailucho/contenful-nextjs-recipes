b1a155491b1b9c1285912bff1a00da613e9f51c9
kaido <luijhy9234@gmail.com>
2022-05-10 21:12:59 -0500

    isr

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export async function getStaticProps() {
   return {
     props: {
       recipes: res.items,
+      revalidate: 1,
     },
   };
 }
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -26,6 +26,7 @@ export async function getStaticProps({ params }) {
 
   return {
     props: { recipe: items[0] },
+    revalidate: 1,
   };
 }
 
