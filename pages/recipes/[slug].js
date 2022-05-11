904d7f56cd9182992c70fb754b8cff36336e2cb4
kaido <luijhy9234@gmail.com>
2022-05-10 21:31:18 -0500

    feat: revalidate

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,38 +1,57 @@
 import { createClient } from "contentful";
-import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
+import Skeleton from "../../components/Skeleton";
+
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 });
+
 export const getStaticPaths = async () => {
-  const res = await client.getEntries({ content_type: "recipe" });
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+
   const paths = res.items.map((item) => {
     return {
       params: { slug: item.fields.slug },
     };
   });
+
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
-export async function getStaticProps({ params }) {
+export const getStaticProps = async ({ params }) => {
   const { items } = await client.getEntries({
     content_type: "recipe",
     "fields.slug": params.slug,
   });
 
+  if (!items.length) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: { recipe: items[0] },
     revalidate: 1,
   };
-}
+};
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
+
   return (
     <div>
       <div className="banner">
